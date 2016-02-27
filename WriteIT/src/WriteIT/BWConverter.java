2525a85bf1a707c5b6bd06e5b823ec62b92e005e
Marko Puza <markopuza@gmail.com>
2016-02-27 20:26:13 +0000

    rotation somehow implemented

--- WriteIT/src/WriteIT/BWConverter.java
+++ WriteIT/src/WriteIT/BWConverter.java
@@ -1,5 +1,9 @@
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -28,21 +32,36 @@ public class BWConverter {
 		}
 		averageGrey /= w*h;
 		
-		double k = 0.50; // correction constant
-		int left = bw.getWidth() - 1;
-	    int right = 0;
-	    int up = bw.getHeight() - 1;
-	    int down = 0;
+		double k = 0.58; // correction constant    
 	    
+	    int[][] neighs = new int[w][h];
 		for (int i = 0; i < w; i++) {
 			for (int j = 0; j < h; j++) {
 				Color c = new Color(in.getRGB(i, j));
 				
 				if (greyValue(c) < k * averageGrey) {
 					bw.setRGB(i,j,0);
-					
-					// set to black
-					if (i > right) {
+
+			          for (int ii = Math.max(0, i - 2); ii < Math.min(w - 1, i + 2); ii++) {
+			        	  for (int jj = Math.max(0, j - 2); jj < Math.min(h - 1, j + 2); jj++) {
+			        		  neighs[ii][jj]++;
+			        	  }
+			          }
+				}
+				else {
+					bw.setRGB(i, j, -1);
+				}
+			}
+		}
+
+		int left = bw.getWidth() - 1;
+	    int right = 0;
+	    int up = bw.getHeight() - 1;
+	    int down = 0;
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < h; j++) {
+				if (neighs[i][j] > 10) {
+					  if (i > right) {
 			            right = i;
 			          }
 			          if (i < left){
@@ -55,26 +74,41 @@ public class BWConverter {
 			            down = j;
 			          }
 				}
-				else {
-					bw.setRGB(i, j, -1);
-				}
 			}
 		}
-
+		
 		
 		// crop the image
-		// System.out.println(left + " " + right + " " + up + " " + down);
 	    BufferedImage cropped = new BufferedImage(right - left + 1, down - up + 1, BufferedImage.TYPE_BYTE_BINARY);
 	    for (int i = 0; i <= right - left; i++) {
 	    	for (int j = 0; j <= down - up; j++) {
-	    		cropped.setRGB(i, j, bw.getRGB(left + i, up + j));
+	    		if (neighs[left + i][up + j] > 5) cropped.setRGB(i, j, 0);
+	    		else cropped.setRGB(i, j, -1);
 	    	}
 	    }
-	    ImageIO.write(cropped, "jpg", new File("//home//marko//workspace//HackLondon//src//bw10.jpg"));
+	   //  ImageIO.write(cropped, "jpg", new File("//home//marko//workspace//HackLondon//src//bww1.jpg"));
+	    
+	    AffineTransform tx = new AffineTransform();
+	    tx.rotate(0.5, cropped.getWidth(), cropped.getHeight());
+
+	    AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+	    cropped = op.filter(cropped, null);
+
+	    BufferedImage stupid = new BufferedImage(cropped.getWidth(), cropped.getHeight(),  BufferedImage.TYPE_BYTE_BINARY);
+	    for (int i = 0; i < cropped.getWidth(); i++) {
+	    	for (int j = 0; j < cropped.getHeight(); j++) {
+	    		if (cropped.getRGB(i, j) > -1) stupid.setRGB(i, j, -1);
+	    		else {
+	    			stupid.setRGB(i, j, cropped.getRGB(i, j));
+	    		}
+	    	}
+	    }
+	    
+	    ImageIO.write(stupid, "jpg", new File("//home//marko//workspace//HackLondon//src//bww1.jpg"));
 	}
 	
 	public static void main(String[] args) throws IOException {
-		File img = new File("//home//marko//workspace//HackLondon//src//10.jpg");
+		File img = new File("//home//marko//workspace//HackLondon//src//w1.jpg");
 		BufferedImage in = ImageIO.read(img);
 		writeBWImage(in);
 	}
