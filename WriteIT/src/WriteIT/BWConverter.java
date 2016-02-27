d0170b4d95ed5afdd8426d896f02fed97013577f
Marko Puza <markopuza@gmail.com>
2016-02-27 17:57:40 +0000

    updated BWConverter

--- WriteIT/src/WriteIT/BWConverter.java
+++ WriteIT/src/WriteIT/BWConverter.java
@@ -1,9 +1,8 @@
-package WriteIT;
-
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
 
 public class BWConverter {
@@ -29,13 +28,32 @@ public class BWConverter {
 		}
 		averageGrey /= w*h;
 		
-		double k = 0.60; // correction constant
+		double k = 0.50; // correction constant
+		int left = bw.getWidth() - 1;
+	    int right = 0;
+	    int up = bw.getHeight() - 1;
+	    int down = 0;
+	    
 		for (int i = 0; i < w; i++) {
 			for (int j = 0; j < h; j++) {
 				Color c = new Color(in.getRGB(i, j));
 				
 				if (greyValue(c) < k * averageGrey) {
 					bw.setRGB(i,j,0);
+					
+					// set to black
+					if (i > right) {
+			            right = i;
+			          }
+			          if (i < left){
+			            left = i;
+			          }
+			          if (j < up) {
+			            up = j;
+			          }
+			          if (j > down) {
+			            down = j;
+			          }
 				}
 				else {
 					bw.setRGB(i, j, -1);
@@ -43,12 +61,21 @@ public class BWConverter {
 			}
 		}
 
-	    ImageIO.write(bw, "jpg", new File("//Users//nathalievonhuth//Desktop//BT3BW.jpg"));
+		
+		// crop the image
+		// System.out.println(left + " " + right + " " + up + " " + down);
+	    BufferedImage cropped = new BufferedImage(right - left + 1, down - up + 1, BufferedImage.TYPE_BYTE_BINARY);
+	    for (int i = 0; i <= right - left; i++) {
+	    	for (int j = 0; j <= down - up; j++) {
+	    		cropped.setRGB(i, j, bw.getRGB(left + i, up + j));
+	    	}
+	    }
+	    ImageIO.write(cropped, "jpg", new File("//home//marko//workspace//HackLondon//src//bw10.jpg"));
 	}
 	
-//	public static void main(String[] args) throws IOException {
-//		File img = new File("//Users//nathalievonhuth//Desktop//BT3.jpg");
-//		BufferedImage in = ImageIO.read(img);
-//		writeBWImage(in);
-//	}
+	public static void main(String[] args) throws IOException {
+		File img = new File("//home//marko//workspace//HackLondon//src//10.jpg");
+		BufferedImage in = ImageIO.read(img);
+		writeBWImage(in);
+	}
 }
